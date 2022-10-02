181fbd1572d29c489a603075b24e712140b98044
patrick mcdowell <112023765+ngrok-patrick@users.noreply.github.com>
2022-10-01 22:15:16 -0700

    Create erase.sh

---
+++ erase.sh
@@ -0,0 +1,6 @@
+echo $api-key
+curl -O https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
+tar xvfz ngrok-v3-stable-linux-arm.tgz 
+./ngrok config add-authtoken $api-key
+cp ./ngrok /usr/bin
+
